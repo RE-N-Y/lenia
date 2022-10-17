058df8c65329cc18cde6ded250f440cdd54a9168
NC <sangwulee2@gmail.com>
2022-10-17 14:57:04 +0000

    add pdf

--- src/lib/engine.test.ts
+++ src/lib/engine.test.ts
@@ -1,8 +1,8 @@
 import * as tf from "@tensorflow/tfjs";
-import { lenia } from "./engine"
+import { lenia, convert, pdf } from "./engine"
 import { describe, expect, it } from 'vitest';
 
-describe("lenia", () => {
+describe("lenia engine", () => {
     const [height, width] = [64,64]
     const [kh, kw] = [8,8]
     const [bumps, channels] = [3,4]
@@ -33,9 +33,37 @@ describe("lenia", () => {
 
     it("should convert tensors to images", () => {
         const world = tf.randomUniform(shape)
-        const image = engine.convert(world)
+        const image = convert(world)
 
         expect(image.shape).toEqual([height, width])
     })
-    
+})
+
+describe("pdf", () => {
+    const [height,width] = [32,32]
+    const [muX, muY] = [16,16]
+    const [sigmaX, sigmaY] = [7,7]
+
+    const yi = tf.linspace(0, height, height)
+    const xi = tf.linspace(0, width, width)
+    const [yy, xx] = tf.meshgrid(yi, xi)
+    const eps = .1
+
+    it("should sum upto normalisation constant", async () => {
+        const p = pdf(yy, xx, [muX,muY], [sigmaX,sigmaY])
+        const [sum] = await p.sum().data()
+        const constant = 2 * Math.PI * Math.sqrt(sigmaX * sigmaY)
+        expect(sum / constant).gt(1 - eps).lt(1 + eps)
+    })
+
+    it("should not leak memory", () => {
+        const last = tf.memory()
+        
+        const p = pdf(yy, xx, [muX,muY], [sigmaX,sigmaY])
+        p.dispose()
+        
+        const current = tf.memory()
+
+        expect(last.numTensors).toEqual(current.numTensors)
+    })
 })
--- src/lib/engine.ts
+++ src/lib/engine.ts
@@ -1,6 +1,78 @@
 import * as tf from '@tensorflow/tfjs';
 
-export const lenia = (kh:number, kw:number, bumps:number, channels:number, speed:number=.1, epsilon:number=4e-8 ) => {
+const eps = tf.scalar(4e-8)
+
+export const pdf = (xi:tf.Tensor, yi:tf.Tensor, mu:number[], sigma:number[]) => {
+    const result = tf.tidy(() => {
+        const [muX, muY] = mu
+        const [sigmaX, sigmaY] = sigma
+
+        const dx = xi.sub(muX).square().div(sigmaX)
+        const dy = yi.sub(muY).square().div(sigmaY)
+        const p = tf.exp(dx.add(dy).div(-2))
+        return p
+    })
+
+    return result
+} 
+
+const radius = (kh:number, kw:number) => {
+    const result = tf.tidy(() => {
+        const yi = tf.linspace(-kh/2, kh/2, kh)
+        const xi = tf.linspace(-kw/2, kw/2, kw)
+        const [yy, xx] = tf.meshgrid(yi, xi)
+        const r = tf.sqrt(yy.square().add(xx.square()))
+
+        return r
+    })
+
+    return result
+}
+
+// tensor to image
+export const convert = (x:tf.Tensor4D) => tf.tidy(() => x.mean([-1,0]).clipByValue(0,1))
+
+// smooth channels
+const filter = tf.tensor([1,3,3,1])
+export const smooth = (x:tf.Tensor4D) => {
+    const result = tf.tidy(() => {
+        const channels = x.shape[3]
+        const kernel:tf.Tensor2D = filter.reshape([1,4]).mul(filter.reshape([4,1]))
+        const nkernel:tf.Tensor4D = kernel.div(kernel.sum()).reshape([4,4,1,1]).tile([1,1,channels,1])
+        const smooooth = tf.depthwiseConv2d(x, nkernel, [1,1], "same")
+        return smooooth
+    })
+
+    return result
+}
+
+const normalise = (x:tf.Tensor, axis?:number[]) => {
+    const result = tf.tidy(() => {
+        const mean = x.mean(axis, true)
+        const variance = mean.squaredDifference(x).mean(axis, true)
+        const normal = x.sub(mean).div(variance.sqrt())
+
+        return normal
+    })
+
+    return result
+}
+
+const growth = (x:tf.Tensor, mu:tf.Tensor, sigma:tf.Tensor) => {
+    const result = tf.tidy(() => {
+        const top = x.sub(mu).square()
+        const bottom = sigma.square().mul(2)
+        const g = tf.exp(top.mul(-1).div(bottom))
+        const gg = g.mul(2).sub(1)
+
+        return gg
+    })
+
+    return result
+
+}
+
+export const lenia = (kh:number, kw:number, bumps:number, channels:number, speed:number=.1) => {
     
     // growth parameters
     const mu = tf.variable(tf.randomUniform([channels]))
@@ -12,72 +84,14 @@ export const lenia = (kh:number, kw:number, bumps:number, channels:number, speed
     const weights = tf.variable(tf.randomUniform([channels, bumps]))
     const linear = tf.variable(tf.randomUniform([channels, channels], -1/Math.sqrt(channels), 1/Math.sqrt(channels)))
 
-    // common parameters
-    const dt = tf.scalar(speed);
-    const eps = tf.scalar(epsilon)
+    // speed
+    const dt = tf.scalar(speed)
 
-    const normalise = (x:tf.Tensor, axis?:number[]) => {
-        const result = tf.tidy(() => {
-            const mean = x.mean(axis, true)
-            const variance = mean.squaredDifference(x).mean(axis, true)
-            const normal = x.sub(mean).div(variance.sqrt())
-
-            return normal
-        })
-
-        return result
-    }
-
-    // smooth channels
-    const filter = tf.tensor([1,3,3,1])
-    const smooth = (x:tf.Tensor4D) => {
-        const result = tf.tidy(() => {
-            const kernel:tf.Tensor2D = filter.reshape([1,4]).mul(filter.reshape([4,1]))
-            const nkernel:tf.Tensor4D = kernel.div(kernel.sum()).reshape([4,4,1,1]).tile([1,1,channels,1])
-            const smooooth = tf.depthwiseConv2d(x, nkernel, [1,1], "same")
-            return smooooth
-        })
-
-        return result
-    }
-    
-    // tensor to image
-    const convert = (x:tf.Tensor4D) => tf.tidy(() => x.mean([-1,0]).clipByValue(0,1))
-
-    const growth = (x:tf.Tensor) => {
-        const result = tf.tidy(() => {
-            const top = x.sub(mu).square()
-            const bottom = sigma.square().mul(2)
-            const g = tf.exp(top.mul(-1).div(bottom))
-            const gg = g.mul(2).sub(1)
-
-            return gg
-        })
-    
-        return result
-    
-    }
-    
-    const radius = () => {
-    
-        const result = tf.tidy(() => {
-            const yi = tf.linspace(-kh/2, kh/2, kh)
-            const xi = tf.linspace(-kw/2, kw/2, kw)
-            const [yy, xx] = tf.meshgrid(yi, xi)
-            const r = tf.sqrt(yy.square().add(xx.square()))
-    
-            return r
-        })
-    
-        return result
-    }
-    
-    
     const constructKernel = (alphas:tf.Tensor, betas:tf.Tensor, weights:tf.Tensor) => {
-    
+
         const result = tf.tidy(() => {
             const [_alphas, _betas, _weights] = [alphas, betas, weights].map(t => t.reshape([1,1,bumps,channels]))
-            const r = radius().reshape([kh, kw, 1, 1]).tile([1,1,bumps,channels])
+            const r = radius(kh, kw).reshape([kh, kw, 1, 1]).tile([1,1,bumps,channels])
             const rd = _betas.mul(r.max([0,1], true))
             const radii = r.div(rd).clipByValue(0,1)    
     
@@ -95,9 +109,9 @@ export const lenia = (kh:number, kw:number, bumps:number, channels:number, speed
 
     const run = (world:tf.Tensor4D) => {
         const result = tf.tidy(() => {
-            const kernel = constructKernel(alphas, betas, weights)
-            const acts = tf.depthwiseConv2d(world, kernel, 1, "same")
-            const update = growth(acts.matMul(linear)).mul(dt)
+            const kernel = constructKernel(alphas, betas, weights, kh, kw, bumps, channels)
+            const acts = tf.depthwiseConv2d(world, kernel, 1, "same").matMul(linear)
+            const update = growth(acts, mu, sigma).mul(dt)
             const noise = tf.randomUniform(world.shape).mul(.001)
             const nextWorld = normalise(world.add(update).add(noise), [1,2])    
 
@@ -107,7 +121,7 @@ export const lenia = (kh:number, kw:number, bumps:number, channels:number, speed
         return result
     }
 
-    return { run, smooth, convert }
+    return { run }
 }
 
     
